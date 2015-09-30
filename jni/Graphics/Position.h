6f1d98a3363463048d2c70027324270fac08eec1
Gaulois94 <serenomickael@gmail.com>
2015-09-30 22:02:15 +0200

    Position struct

---
+++ jni/Graphics/Position.h
@@ -0,0 +1,14 @@
+#ifndef DEF_POSITION
+#define DEF_POSITION
+
+/** \brief Enumerate for rectangle position*/
+struct Position
+{
+	TopRight,
+	TopLeft,
+	Center,
+	BottomRight,
+	BottomLeft
+}
+
+#endif
