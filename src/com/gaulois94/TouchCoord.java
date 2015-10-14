e41ce0667312d33043f6f8277e6b9aee2985fdeb
Gaulois94 <serenomickael@gmail.com>
2015-10-14 16:26:09 +0200

    Add java interface

---
+++ src/com/gaulois94/TouchCoord.java
@@ -0,0 +1,14 @@
+package com.gaulois94;
+
+private class TouchCoord
+{
+	public static final UP   = 1;
+	public static final DOWN = 2;
+	public static final MOVE = 3;
+
+	public float x           = 0.0;
+	public float y           = 0.0;
+	public float startX      = 0.0;
+	public float startY      = 0.0;
+	public int type          = UP;
+}
