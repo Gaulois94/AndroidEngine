702c20dfc361d38671cfda4ce57cebeb9fb9e8ce
Gaulois94 <serenomickael@gmail.com>
2015-10-14 16:28:36 +0200

    fix touchCoord compilation error

--- jni/EventManager.cpp
+++ jni/EventManager.cpp
@@ -1,5 +1,16 @@
 #include "EventManager.h"
 
 //Initialize the TouchCoord struct.
-touchCoord.down   = false;
-touchCoord.startX = touchCoord.startY = touchCoord.x = touchCoord.y = 0;
+TouchCoord touchCoord = {.type=UP, .startX=0, .startY=0, .x=0, .y=0};
+
+bool touchInRect(const Rectangle3f& rect)
+{
+	return (touchCoord.x > rect.x && touchCoord.x < rect.x + rect.width &&
+			touchCoord.y > rect.y && touchCoord.y < rect.y + rect.height);
+}
+
+bool touchInRect(const Rectangle2f& rect)
+{
+	return (touchCoord.x > rect.x && touchCoord.x < rect.x + rect.width &&
+			touchCoord.y > rect.y && touchCoord.y < rect.y + rect.height);
+}
