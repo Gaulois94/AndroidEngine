98c3f2b2c914d435a09d15cbfb32c091ce499a6a
Gaulois94 <serenomickael@gmail.com>
2015-10-20 19:58:37 +0200

    Update

--- jni/nativeUpdatable.cpp
+++ jni/nativeUpdatable.cpp
@@ -2,64 +2,65 @@
 
 JNIEXPORT jlong   JNICALL Java_com_gaulois94_Updatable_createUpdatable(JNIEnv* jenv, jclass jcls, jlong parent)
 {
-	return (jlong)(new Updatable(parent));
+	Updatable* u = new Updatable((Updatable*)parent);
+	return (jlong)(u);
 }
 
-JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateFocusUpdatable(JNIEnv* jenv, jlong ptr, jlong renderer)
+JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateFocusUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong renderer)
 {
 	Updatable* u = (Updatable*)ptr;
-	u->updateFocus((Renderer*)renderer);
+	u->updateFocus(*(Render*)renderer);
 }
 
-JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onFocusUpdatable(JNIEnv* jenv, jlong ptr, jlong renderer)
+JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onFocusUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong renderer)
 {
 	Updatable* u = (Updatable*)ptr;
-	u->onFocus((Renderer*)renderer);
+	u->onFocus(*(Render*)renderer);
 }
 
-JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateUpdatable(JNIEnv* jenv, jlong ptr, jlong render)
+JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong render)
 {
 	Updatable* u = (Updatable*)ptr;
-	u->update((Render*)render);
+	u->update(*(Render*)render);
 }
 
-JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onUpdateUpdatable(JNIEnv* jenv, jlong ptr, jlong render)
+JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onUpdateUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong render)
 {
 	Updatable* u = (Updatable*)ptr;
-	u->onUpdate((Render*)render);
+	u->onUpdate(*(Render*)render);
 }
 
-JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_addChildUpdatable(JNIEnv* jenv, jlong ptr, jlong child, int pos)
+JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_addChildUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong child, int pos)
 {
 	Updatable* u = (Updatable*)ptr;
 	u->addChild((Updatable*)child, pos);
 }
 
-JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_setParentUpdatable(JNIEnv* jenv, jlong ptr, jlong parent, int pos)
+JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_setParentUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong parent, int pos)
 {
 	Updatable* u = (Updatable*)ptr;
 	u->setParent((Updatable*)parent, pos);
 }
 
-JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildUpdatable(JNIEnv* jenv, jlong ptr, jlong child)
+JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong child)
 {
 	Updatable* u = (Updatable*)ptr;
 	return u->removeChild((Updatable*)child);
 }
 
-JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildUpdatable(JNIEnv* jenv, jlong ptr, int pos)
+JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildPosUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, int pos)
 {
 	Updatable* u = (Updatable*)ptr;
 	return u->removeChild(pos);
 }
 
-JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_isChildUpdatable(JNIEnv* jenv, jlong ptr, jlong child)
+JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_isChildUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong child)
 {
 	Updatable* u = (Updatable*)ptr;
-	return u->isChild(child);
+	return u->isChild((Updatable*)child);
 }
 
-JNIEXPORT jobject JNICALL Java_com_gaulois94_Updatable_getParentUpdatable(JNIEnv* jenv, jlong ptr)
+JNIEXPORT jobject JNICALL Java_com_gaulois94_Updatable_getParentUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr)
 {
 	Updatable* u = (Updatable*)ptr;
 	return u->getParent()->getJobject();
--- jni/nativeUpdatable.h
+++ jni/nativeUpdatable.h
@@ -1,19 +1,23 @@
 #ifndef DEF_NATIVEUPDATBLE_INCLUDE
 #define DEF_NATIVEUPDATBLE_INCLUDE
 
+#include <jni.h>
+#include "Updatable.h"
+#include "Render.h"
+
 extern "C"
 {
 	JNIEXPORT jlong   JNICALL Java_com_gaulois94_Updatable_createUpdatable(JNIEnv* jenv, jclass jcls, jlong parent);
-	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateFocusUpdatable(JNIEnv* jenv, jlong ptr, jlong renderer);
-	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onFocusUpdatable(JNIEnv* jenv, jlong ptr, jlong renderer);
-	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateUpdatable(JNIEnv* jenv, jlong ptr, jlong render);
-	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onUpdateUpdatable(JNIEnv* jenv, jlong ptr, jlong render);
-	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_addChildUpdatable(JNIEnv* jenv, jlong ptr, jlong child, int pos);
-	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_setParentUpdatable(JNIEnv* jenv, jlong ptr, jlong parent, int pos);
-	JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildUpdatable(JNIEnv* jenv, jlong ptr, jlong child);
-	JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildUpdatable(JNIEnv* jenv, jlong ptr, int pos);
-	JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_isChildUpdatable(JNIEnv* jenv, jlong ptr, jlong child);
-	JNIEXPORT jobject JNICALL Java_com_gaulois94_Updatable_getParentUpdatable(JNIEnv* jenv, jlong ptr);
+	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateFocusUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong renderer);
+	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onFocusUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong renderer);
+	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_updateUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong render);
+	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_onUpdateUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong render);
+	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_addChildUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong child, int pos);
+	JNIEXPORT void    JNICALL Java_com_gaulois94_Updatable_setParentUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong parent, int pos);
+	JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong child);
+	JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_removeChildPosUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, int pos);
+	JNIEXPORT bool    JNICALL Java_com_gaulois94_Updatable_isChildUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr, jlong child);
+	JNIEXPORT jobject JNICALL Java_com_gaulois94_Updatable_getParentUpdatable(JNIEnv* jenv, jobject jobj, jlong ptr);
 }
 
 #endif
