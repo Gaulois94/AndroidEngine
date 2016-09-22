95b84140aa8c850c176eabe4465ba2a58c153eee
Gaulois94 <serenomickael@gmail.com>
2016-09-22 15:47:44 +0000

    Interface for handling texts

---
+++ jni/TextInterface.cpp
@@ -0,0 +1,80 @@
+#include "TextInterface.h"
+
+TextInterface::TextInterface(const Font* font, const char* text) : m_font(font), m_text(NULL)
+{
+	setText(text);
+}
+
+TextInterface::~TextInterface()
+{
+	if(m_text)
+		free(m_text);
+}
+
+void TextInterface::setFont(const Font* font)
+{
+	m_font = font;
+}
+
+void TextInterface::setText(const char* text)
+{
+	if(text != m_text)
+	{
+		if(m_text != NULL)
+			free(m_text);
+
+		//We copy the new text and store it
+		m_text = (char*) malloc((strlen(text)+1)*sizeof(char));
+		strcpy(m_text, text);
+	//	m_text[strlen(text)] = '\0';
+	}
+}
+
+const Font* TextInterface::getFont() const
+{
+	return m_font;
+}
+
+const char* TextInterface::getText() const
+{
+	return m_text;
+}
+
+void TextInterface::addChar(uint8_t c, int pos)
+{
+	if(pos == -1)
+		pos = strlen(m_text);
+
+	char* text = (char*)malloc((strlen(m_text)+2)*sizeof(char));
+	strcpy(text, m_text);
+
+	if(c=='\b')
+	{
+		LOG_ERROR("BACKSPACE");
+		for(int i=pos-1; i < strlen(text); i++)
+			text[i] = text[i+1];
+	}
+
+	//DEL command
+	else if(c==0x7f)
+	{
+		for(int i=pos; i < strlen(text); i++)
+			text[i] = text[i+1];
+	}
+
+	else if(c >= ' ' && c <= '~')
+	{
+		for(int i=strlen(text)+1; i > pos; i--)
+			text[i] = text[i-1];
+		text[pos] = c;
+	}
+
+	else
+	{
+		free(text);
+		return;
+	}
+
+	setText(text);
+	free(text);
+}
---
+++ jni/TextInterface.h
@@ -0,0 +1,41 @@
+#ifndef  TEXTINTERFACE_INC
+#define  TEXTINTERFACE_INC
+
+#include<cstring>
+#include "Font.h"
+
+class TextInterface
+{
+	public:
+		TextInterface(const Font* font, const char* text);
+
+		~TextInterface();
+
+		/** \brief set the font of the text
+		 * \param font the new font. The user should store the font somewhere (for example on a ResourcesManager) */
+		virtual void setFont(const Font* font);
+
+		/** \brief set the text string.
+		 * \param text the new string */
+		virtual void setText(const char* text);
+
+		/** \brief get the text font.
+		 * \return the font associated with the text.*/
+		const Font* getFont() const;
+
+		/** \brief get the text string.
+		 * \return the text string.*/
+		const char* getText() const;
+
+		/** \brief add a character to the text.
+		 * Following it's value, it can be rejected (like shift control), removing char (backspace, delete) or adding the char at the position
+		 * \param c the character to add at the position pos
+		 * \param pos the position where to add the character. -1 = at the end of the text*/
+		void addChar(uint8_t c, int pos=-1);
+	protected:
+		const Font*  m_font;
+		char*        m_text;
+
+};
+
+#endif
